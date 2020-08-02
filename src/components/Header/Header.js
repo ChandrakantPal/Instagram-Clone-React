9a5a2c4db217505f1d6262d9b347f299a48f03a1
sanjay <sanjay161097@gmail.com>
2020-08-02 21:20:52 +0530

    header component + styling

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const header = (props) => {
+  return (
+    <header className="app__header">
+      <img
+        className="app__header__logo"
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default header;
